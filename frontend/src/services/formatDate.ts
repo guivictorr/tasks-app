b2947bd098d6bf37136a8994e3c8aba10be9d2a3
Guilherme <guilhermevictor20@gmail.com>
2020-09-21 11:04:25 -0300

    mudança no serviço de formatar data

--- frontend/src/services/formatDate.ts
+++ frontend/src/services/formatDate.ts
@@ -1,12 +1,5 @@
 function formatDate(date: string) {
-  let dateArr = date.split('T')[0].split('-', 3)
-
-  let year = dateArr[0]
-  let month = dateArr[1]
-  let day = dateArr[2]
-
-  let formatedDate = `${day}/${month}/${year}`
-  
+  const formatedDate = new Date(date).toLocaleDateString("pt-br");
   return formatedDate
 }
 
