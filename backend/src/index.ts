f2788ddad6b63e29f328bbc22b7d678427cd8d0c
Guilherme <guilhermevictor20@gmail.com>
2020-09-30 20:27:45 -0300

    removed nodemon, added ts-node-dev

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
 import { createConnection } from 'typeorm'
-import * as express from "express";
+import express from "express";
 const cors = require('cors')
-import * as bodyParser from 'body-parser'
+import bodyParser from 'body-parser'
 import routes from "./routes";
 
 const app = express()
